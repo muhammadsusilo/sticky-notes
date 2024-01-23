cba68208f183a6a0e27943518da966b1c87ccf8a
Muhammad Susilo <muhammadsusilo43@gmail.com>
2024-01-23 21:05:47 +0700

    No Fixed function createNote

--- script.js
+++ script.js
@@ -1,22 +1,36 @@
-function get() {
+const appSticky = document.getElementById("sticky");
+const addNoteSticky = document.querySelector(".add-note")
+
+function getNote() {
+    return JSON.parse(localStorage.getItem("stickyFile-notes") || "[]")
+}
+
+function saveNote(notes) {
+    localStorage.setItem("stickyFile-notes", JSON.stringify(notes))
+}
+
+function createNote(id, content) {
+    const textArea = document.createElement("textarea");
+
+    textArea.classList.add("note");
+    textArea.value = content;
+    textArea.placeholder = "Empty Sticky Note";
+
+    textArea.addEventListener("change", () => {
+        updateNote(id, textArea.value);
+    });
+
+    return textArea;
+}
+
+
+function addNote() {
 
 }
 
-function save() {
-
-}
-
-function create(id, content) {
-
-}
-
-
-function add() {
-
-}
-
-function update(id, newContent) {
-
+function updateNote(id, newContent) {
+    console.info("update");
+    console.info(id, newContent);
 }
 
 function deleteNote(id, element) {
